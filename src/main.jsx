70f7923bd3870eb7269e5c77c62f6d875d52579b
Rajnish2105 <166371359+Rajnish2105@users.noreply.github.com>
2024-12-01 01:01:37 +0530

    updated vite.config file

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import AllMeetUps, { AllMeetupsLoader } from "./Pages/AllMeetUps";
-import NewMeetUps from "./Pages/NewMeetups.jsx";
+import NewMeetUps from "./Pages/NewMeetups";
 import Favorites from "./Pages/Favorites";
 import Layout from "./Components/Layout/Layout";
 import MeetupDetails, {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
+});
