a4236565d6ad565e53a85a97df5706258f319eaf
Rajnish2105 <166371359+Rajnish2105@users.noreply.github.com>
2024-12-01 09:36:16 +0530

    Fix missing file

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import AllMeetUps, { AllMeetupsLoader } from "@/Pages/AllMeetUps";
-import NewMeetUps from "@/Pages/NewMeetups";
+import NewMeetUps from "@/Pages/NewMeetUps";
 import Favorites from "@/Pages/Favorites";
 import Layout from "@/Components/Layout/Layout";
 import MeetupDetails, {
